29c01631d8457e9ce83bbee4de581ac1fbf19b0c
Scofield626 <scofield_zliu@outlook.com>
2026-01-05 15:25:56 +0000

    perf: fastId optimization from Caracal

--- remora/src/proxy/core.rs
+++ remora/src/proxy/core.rs
@@ -415,7 +415,6 @@ where
             // check the version ID for shared objects
             // skip if versions don't match
 
-            let now = std::time::Instant::now();
             let ready_to_execute =
                 !transaction.input_objects().iter().any(|input_object| {
                     matches!(
@@ -447,8 +446,6 @@ where
                 ExecutionResults::<E>::new(transaction.deref().clone(), None, None)
             };
 
-            let elapsed = now.elapsed();
-            tracing::info!("Execution time: {}", elapsed.as_micros());
             tracing::debug!(
                 "Proxy {} completed execution for transaction {:?}, success: {}",
                 id,
